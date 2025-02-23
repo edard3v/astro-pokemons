1598fe077259b98e5608d52709399b9df110b1af
edard3v <edard3v@gmail.com>
2025-02-23 10:02:14 -0500

    agg seo

---
+++ src/layouts/Layout/seo.ts
@@ -0,0 +1,7 @@
+export const SEO = {
+  description: "Mostrando información sobre pokemons",
+  keywords: ["pokemon, pokemons"],
+  author: "edard3v",
+  img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  base_url: "http://localhost:4321/",
+};
