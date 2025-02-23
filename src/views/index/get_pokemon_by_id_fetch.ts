a87f086637ed3395ebe80a103ccc03172271f58a
edard3v <edard3v@gmail.com>
2025-02-23 02:53:39 -0500

    funciona

--- src/views/index/get_pokemon_by_id_fetch.ts
+++ src/views/index/get_pokemon_by_id_fetch.ts
@@ -1,3 +1,11 @@
+export const get_pokemon_by_id_fetch = async (id: number): Promise<GetPokemonByIdFetch> => {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+
+  if (!res.ok) throw TypeError("Algo fue mal con get_pokemons_by_id_fetch");
+
+  return await res.json();
+};
+
 export interface GetPokemonByIdFetch {
   abilities: Ability[];
   base_experience: number;
--- src/views/index/get_pokemons_fetch.ts
+++ src/views/index/get_pokemons_fetch.ts
@@ -1,9 +1,24 @@
+import { get_pokemon_by_id_fetch } from "./get_pokemon_by_id_fetch";
+
 export const get_pokemons_fetch = async (): Promise<GetPokemonsFetch> => {
   const res = await fetch("https://pokeapi.co/api/v2/pokemon");
 
   if (!res.ok) throw TypeError("Algo fue mal con get_pokemons_fetch");
 
-  return await res.json();
+  const pokemons = (await res.json()) as GetPokemonsFetch;
+
+  const pokemon_details = await Promise.all(
+    pokemons.results.map((result) => get_pokemon_by_id_fetch(Number(result.url.split("/").at(-2))))
+  );
+
+  const new_pokemons_results = pokemons.results.map((result) => {
+    const result_detail = pokemon_details.find((pokemon) => pokemon.name === result.name);
+    const img = result_detail?.sprites.other?.showdown.front_default;
+
+    return { ...result, img };
+  });
+
+  return { ...pokemons, results: new_pokemons_results };
 };
 
 export interface GetPokemonsFetch {
@@ -16,4 +31,5 @@ export interface GetPokemonsFetch {
 export interface GetPokemonsResult {
   name: string;
   url: string;
+  img?: string;
 }
